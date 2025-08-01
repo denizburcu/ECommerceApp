a7585906023555f645fbac06f8a3058392dfdf65
denizburcu <denizburcua@gmail.com>
2025-08-01 18:42:25 +0300

    Initial commit

---
+++ ECommerceApp.Api/Program.cs
@@ -0,0 +1,27 @@
+﻿using ECommerceApp.Application.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register services in the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register application layer services
+builder.Services.AddApplicationServices();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
